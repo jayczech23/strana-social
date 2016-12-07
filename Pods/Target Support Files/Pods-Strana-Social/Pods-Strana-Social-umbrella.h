78723591b28d702bb3510c2dbac5e0afc9587c5f
Jordan Cech <jordancech@Jordans-MBP-2.casacisco.local>
2016-12-06 22:55:05 -0500

    Setup firebase and pods.

---
+++ Pods/Target Support Files/Pods-Strana-Social/Pods-Strana-Social-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Strana_SocialVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Strana_SocialVersionString[];
+
---
+++ Strana-Social/SignInVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Strana-Social
+//
+//  Created by Jordan Cech on 12/6/16.
+//  Copyright © 2016 Strana LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
+
