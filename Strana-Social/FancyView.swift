ba0b43f89b44c47b73171f55ccec7719111c8af7
Jordan Cech <jordancech@Jordans-MBP-2.casacisco.local>
2016-12-07 16:07:22 -0500

    updated UI color.

---
+++ Strana-Social/FancyView.swift
@@ -0,0 +1,26 @@
+//
+//  FancyView.swift
+//  Strana-Social
+//
+//  Created by Jordan Cech on 12/7/16.
+//  Copyright © 2016 Strana LLC. All rights reserved.
+//
+
+import UIKit
+
+class FancyView: UIView {
+
+//-----------------------------------------------------------------
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // add shadowing to the view
+        layer.shadowColor = UIColor(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
+        
+        
+    }
+//-----------------------------------------------------------------
+ 
+
+}
