80eebedd68f3d6c23addff381d804ebed331950a
Jordan Cech <jordancech@Jordans-MBP-2.casacisco.local>
2016-12-09 17:07:15 -0500

    Updated code formatting

--- Strana-Social/AppDelegate.swift
+++ Strana-Social/AppDelegate.swift
@@ -65,7 +65,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
         
         return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
-        
     }
 }
 
