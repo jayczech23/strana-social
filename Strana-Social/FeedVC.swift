75486f3fac0e511722fee70e97732dbc1480b683
Jordan Cech <jordancech@Jordans-MBP-2.casacisco.local>
2016-12-22 00:49:32 -0500

    Completed FeedVC UI.

--- Strana-Social/FeedVC.swift
+++ Strana-Social/FeedVC.swift
@@ -11,19 +11,33 @@ import SwiftKeychainWrapper
 import Firebase
 
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        tableView.delegate = self
+        tableView.dataSource = self
         
     }
-    
+//----------------------------------------------------------------
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+//----------------------------------------------------------------
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+//----------------------------------------------------------------
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+    }
 //----------------------------------------------------------------
     
-    // Sign out of firebase and remove keychain.
-    @IBAction func signOutTapped(_ sender: Any) {
+    
+    @IBAction func signOutBtnTapped(_ sender: Any) {
         
         let keychainResult = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
         print("JAY: Remove from keychain? \(keychainResult)")
---
+++ Strana-Social/PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  Strana-Social
+//
+//  Created by Jordan Cech on 12/21/16.
+//  Copyright © 2016 Strana LLC. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        //
+        
+        
+    }
+
+
+
+}
